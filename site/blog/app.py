7685b183f54c95d803f018fa2fa1cba295599ddf
Chance <chance_st@yahoo.com>
2018-05-16 11:24:50 -0700

    start

--- site/blog/app.py
+++ site/blog/app.py
@@ -1,2 +1,12 @@
 
 import pymongo
+
+uri = "mongodb://127.0.0.1:27017"
+
+client = pymongo.MongoClient(uri)
+database = client['fullstack']
+collection = database['students']
+
+students = [ student for student in collection.find({})]
+
+print(students)
