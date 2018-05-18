e0bef2128efb44255f053694640c2f7456f4e766
Chance <chance_st@yahoo.com>
2018-05-17 22:37:05 -0700

    adds

--- site/blog/app.py
+++ site/blog/app.py
@@ -1,8 +1,16 @@
 
 import pymongo
 
-uri = "mongodb://127.0.0.1:27017"
+from models.database import Database
+from models.post import Post
 
+__author__ = 'chance'
+
+Database.initialize()
+
+#uri = "mongodb://127.0.0.1:27017"
+
+# Start mongod from Git CMD
 client = pymongo.MongoClient(uri)
 database = client['fullstack']
 collection = database['students']
@@ -10,3 +18,9 @@ collection = database['students']
 students = [ student for student in collection.find({})]
 
 print(students)
+
+post = Post("A", "B")
+post2 = Post()
+
+print( post.content)
+
--- site/blog/models/post.py
+++ site/blog/models/post.py
@@ -1,2 +1,27 @@
+from models.database import Database
+
 __author__ = 'chance'
 
+class Post(object):
+
+    def __init__(self, id, blog_id, date, author=__author__, title="Title", content="DC"):
+        self.title = title
+        self.content = content
+        self.author = author
+        self.id = id
+        self.blog_id = id
+        self.created_date = date
+
+    def save_to_mongo(self):
+        Database.insert(collection = 'posts',
+                        data = self.json())
+
+    def json(self):
+        return{
+            'id': self.id,
+            'blog_id': self.blog_id,
+            'author': self.author,
+            'content': self.content,
+            'title': self.title,
+            'created_date': self.created_date
+        }
