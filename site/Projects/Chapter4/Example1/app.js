d4902b2e620a4e1166a732628c7a9acb6ba46173
Chance <chance_st@yahoo.com>
2018-06-15 11:30:03 -0700

    js1

--- site/Projects/Chapter4/Example1/app.js
+++ site/Projects/Chapter4/Example1/app.js
@@ -1,7 +1,4 @@
-var main = function() {
-    "use strict";
-    document.write('<h3>' + 'greeting' + '</h3>')
-    window.alert("hello world");
-};
 
-$(document).ready(main);
+
+document.write('<h3>' + 'greeting' + '</h3>')
+window.alert("hello world");
