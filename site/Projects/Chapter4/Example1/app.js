d91c70d82620c8992ace0100985d9f78a323fa87
Chance <chance_st@yahoo.com>
2018-06-19 11:40:15 -0700

    js1\

--- site/Projects/Chapter4/Example1/app.js
+++ site/Projects/Chapter4/Example1/app.js
@@ -1,5 +1,14 @@
-var greeting = 'hello 5';
-var today = new Date();
+//var greeting = 'hello 5';
+//var today = new Date();
 
-document.write('<h3>' + greeting + " " + today + " !! " + '</h3>')
-window.alert("hello world3");
+var main = function() {
+   "use strict";
+   window.alert("hello world 1");
+};
+ 
+$(document).ready(main);
+ 
+//document.write('<h3>' + 'greeting' + '</h3>')
+
+//document.write('<h3>' + greeting + " " + today + " !! " + '</h3>')
+//window.alert("hello world3");
