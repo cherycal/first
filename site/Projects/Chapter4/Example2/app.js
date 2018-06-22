5163e3ed516f8e5b5cc05b90dda81bdb89cf510c
Chance <chance_st@yahoo.com>
2018-06-22 11:37:23 -0700

    js1\

--- site/Projects/Chapter4/Example2/app.js
+++ site/Projects/Chapter4/Example2/app.js
@@ -1,17 +1,13 @@
-//var greeting = 'hello 5';
-//var today = new Date();
+
 
 var main = function() {
 	"use strict";
-	var greeting = 'hello 5';
-	var today = new Date();
-	document.write('<h3>' + greeting + " " + today + " !! " + '</h3>')
-	window.alert("hello world3");
+
+	$(".comment-input button").on("click", function(event)){
+		console.log("Hello world!");
+	}
+	
+
 };
  
 $(document).ready(main);
- 
-//document.write('<h3>' + 'greeting' + '</h3>')
-
-//document.write('<h3>' + greeting + " " + today + " !! " + '</h3>')
-//window.alert("hello world3");
