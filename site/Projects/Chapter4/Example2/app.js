82f4076c251a9974dbd77c3f63d7cde048909cf4
Chance <chance_st@yahoo.com>
2018-06-28 10:11:56 -0700

    js1\

--- site/Projects/Chapter4/Example2/app.js
+++ site/Projects/Chapter4/Example2/app.js
@@ -4,7 +4,8 @@ var main = function() {
 	"use strict";
 
 	$(".comment-input button").on("click", function(event){
-		console.log("Hello world!");
+		//console.log("Hello world!");
+		$(".comments").append("<p>this is a new comment</p>");
 	});
 	
 
