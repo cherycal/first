ecfe78ac2da96eeacbc55f38d1e62c731e0ac94d
Chance <chance_st@yahoo.com>
2018-06-28 16:12:45 -0700

    js1\

--- site/Projects/Chapter4/Example2/app.js
+++ site/Projects/Chapter4/Example2/app.js
@@ -4,13 +4,22 @@ var main = function() {
 	"use strict";
 
 	$(".comment-input button").on("click", function(event){
-		//console.log("Hello world!");
-		//$(".comments").append("<p>this is a new comment</p>");
-		//var $new_comment = $("<p>").text("this too is a new comment");
+		
 		if ($(".comment-input input").val() !== "") {
 			var $new_comment = $("<p>").text($(".comment-input input").val())
 			$(".comments").append($new_comment);
 			$(".comment-input input").val("")
+		}
+	});
+
+	$(".comment-input input").on("keypress", function(event){
+		//var $new_comment;
+		if( event.keyCode === 13 ) {
+			if ($(".comment-input input").val() !== "") {
+				var $new_comment = $("<p>").text($(".comment-input input").val())
+				$(".comments").append($new_comment);
+				$(".comment-input input").val("")
+			}
 		}
 	});
 	
