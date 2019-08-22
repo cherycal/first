6fca09e0fe5d5e6356fb3f4f3f0125045f2239b7
Chance <chance_st@yahoo.com>
2019-08-22 09:30:45 -0700

    draft

--- site/push_notification/push.py
+++ site/push_notification/push.py
@@ -1,17 +1,61 @@
 __author__ = 'chance'
 
 from pyfcm import FCMNotification
+import time
 
-push_service = FCMNotification(api_key="AAAARAUK_1U:APA91bEWDFmhqWVEicI1xWh7R41lB8DGyjiRrLlfaa-CqLtMLvbGzLtL6nCBYgXVKKuiLas8hsX6YnHFQUoWqHZS_crAssz2B-msCzOAYqWqsTuc9AgPnTJL0OtPnEjBG9FC4hFd9339")
+class Push(object):
+
+    message_body: str
+
+    def __init__(self):
+        api_key = "AAAARAUK_1U:APA91bEWDFmhqWVEicI1xWh7R41lB8DGyjiRrLlfaa" \
+                       "-CqLtMLvbGzLtL6nCBYgXVKKuiLas8hsX6YnHFQUoWqHZS_crAssz2B" \
+                       "-msCzOAYqWqsTuc9AgPnTJL0OtPnEjBG9FC4hFd9339"
+        reg_id = "fyOoafoZVl8:APA91bHk3CwpzBXTKbEZiFs6i57NZqnSDPrkA0vZI" \
+                      "-rUbMmK6t1ov9bhqFULtLOUAfi0BXs0y4VCoRiu1nBdo82NK7iDCcIkMnV7eqpTDOP3a9X3bmMCPb4gk0OSIGPl5UANaOKFQjNl"
+        self.push_service = FCMNotification(api_key=api_key)
+        self.registration_id = reg_id
+        self.message_title = "Python test 1"
+        self.message_body = "Hello python test 1"
+        self.res = {}
+        self.interval = 0
+        self.title = None
+        self.body = None
 
 
-print("Hello")
+    def push(self, title="None", body="None"):
+        res = self.push_service.notify_single_device(registration_id=self.registration_id,
+                                                         message_title=title,
+                                                         message_body=body, sound="whisper.mp3",
+                                                         badge="Test2")
+        return res
 
-# Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
+    def set_msg(self,title,body):
+        self.title = title
+        self.body = body
 
-registration_id = "fyOoafoZVl8:APA91bHk3CwpzBXTKbEZiFs6i57NZqnSDPrkA0vZI-rUbMmK6t1ov9bhqFULtLOUAfi0BXs0y4VCoRiu1nBdo82NK7iDCcIkMnV7eqpTDOP3a9X3bmMCPb4gk0OSIGPl5UANaOKFQjNl"
-message_title = "Python test 1"
-message_body = "Hello python test 1"
-result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body,sound="Default", topic_name="Test2")
+    def set_interval(self, interval):
+        self.interval = interval
 
-print (result)
+    def push_number(self, number):
+        for i in range(0,number):
+            self.push(self.title, self.body)
+            time.sleep(4)
+
+    def push_change(self, number, title="None", body="None"):
+        self.title = title
+        self.body = body
+        if(number < 0):
+            self.push_number(1)
+        elif (number > 0):
+            self.push_number(2)
+
+        time.sleep(10)
+
+        if( abs(number) < 6000 ):
+            self.push_number( int( (abs(number)+1 )/ 2 ) )
+
+
+
+
+
