7a54c3abb5bb75eee3eca6f2e0080fe94839c1e8
Chance <chance_st@yahoo.com>
2019-08-22 08:52:11 -0700

    more stuff

--- site/push_notification/rosters_w_changes.py
+++ site/push_notification/rosters_w_changes.py
@@ -1,19 +1,26 @@
 __author__ = 'chance'
 
 import sqldb
+import tools
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
+from datetime import datetime
 
 import push
 inst = push.Push()
 
+platform = tools.get_platform()
 bdb = sqldb.DB('bdb.db')
+
 
 league_dict = {}
 team_dict = {}
 old_rosters = {}
 new_rosters = {}
+now = datetime.now() # current date and time
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 msg = ""
 
 c = bdb.select("SELECT * FROM Leagues")
@@ -32,20 +39,28 @@ for t in c:
 c = bdb.delete("DELETE FROM Rosters")
 c = bdb.select("SELECT * FROM Leagues where LeagueID != 14047614")
 
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+# if (platform == "Windows" ):
+#     driver = webdriver.Chrome('C:/Users/chery/chromedriver.exe', chrome_options=options )
+# elif (platform == "linux"):
+#     driver = webdriver.Chrome( chrome_options=options)
+# else:
+#     print("Platform " + platform + " not recognized. Exiting.")
+#     exit(-1)
 
-driver = webdriver.Chrome('C:/Users/chery/chromedriver.exe')
+driver = tools.get_driver()
 
 
 for t in c:
     url = "http://fantasy.espn.com/baseball/league/rosters?leagueId=" + str(t[0])
     print (url)
     driver.get(url)
-    time.sleep(5)
+    time.sleep(12)
 
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
-
-    # time.sleep(1)
 
     sections = soup.find_all("section")
     for section in sections:
@@ -67,11 +82,24 @@ for t in c:
                               "\" ,\"" \
                                                                                        + \
                               name.text + "\"," + str(t[0]) + ")"
-                    print(command)
+                    #print(command)
                     new_rosters[row[0]+':'+name.text] = name.text
                     bdb.insert(command)
 
-
+c = bdb.select("SELECT count(Player) from Rosters")
+players = c[0][0]
+    
+if (int(players) < 1145 ):
+    print("Not enough players in Rosters: " + str(players))
+    msg += "Not enough players in Rosters"
+    inst.push("Roster error: "+str(date_time), msg)
+    time.sleep(2)
+    inst.push("Roster error: "+str(date_time), msg)
+    time.sleep(2)
+    inst.push("Roster error: "+str(date_time), msg)
+else:
+    print("Rosters appear to be full: " + str(players))
+                    
 for p in old_rosters:
     if new_rosters.get(p):
         if ( old_rosters[p] == new_rosters[p] ):
@@ -96,8 +124,15 @@ for p in new_rosters:
 
 if (msg != ""):
     print("Msg: " + msg)
-    inst.push("Roster changes", msg)
+    inst.push("Roster changes: "+str(date_time), msg)
+    time.sleep(2)
+    inst.push("Roster changes: "+str(date_time), msg)
+else:
+    msg = "No changes"
+    print("Msg: " + msg)
+    inst.push("No changes: "+str(date_time), msg)
 
-driver.close()
+
+driver.quit()
 bdb.close()
 
