f78130635a9181fbe71898928a6bfe4a1c3caec2
chance <chance_st@yahoo.com>
2019-08-30 13:24:56 -0700

    rosters.bash

--- site/push_notification/rosters.bash
+++ site/push_notification/rosters.bash
@@ -3,4 +3,4 @@
 CURRENTEPOCTIME=`date +"%Y%m%d-%H%M%S"`
 
 echo $CURRENTEPOCTIME
-/usr/bin/python3 /media/sf_Shared/first/site/push_notification/rosters_w_changes.py > /media/sf_Shared/first/site/push_notification/logs/roster_changes_log_$CURRENTEPOCTIME 2>&1
+/usr/bin/python3 /media/sf_Shared/first/site/push_notification/rosters_w_changes.py > /media/sf_Shared/first/site/push_notification/logs/roster_changes_log_$CURRENTEPOCTIME.txt 2>&1
