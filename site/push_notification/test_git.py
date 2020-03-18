823034c3ab28e582c8045f0d2d7383628293e640
chance <chance_st@yahoo.com>
2020-03-18 10:45:03 -0700

    stub

--- site/push_notification/test_git.py
+++ site/push_notification/test_git.py
@@ -1,3 +1,28 @@
+from git import Repo
+
+# rorepo is a Repo instance pointing to the git-python repository.
+# For all you know, the first argument to Repo is a path to the repository
+# you want to work with
+# To find .git directory run git rev-parse --show-toplevel
+# /media/sf_Shared/first
+
+repo = Repo("/Ubuntu/Shared/first/")
+assert not repo.bare
+
+file = "site/push_notification/file.txt"
+
+git = repo.git
+
+git.add(file)
+
+git.commit('-m','update',file)
+
+git.push()
+
+print( git.status() )
+
+
+
 __author__ = 'chance'
 
 print("1")
